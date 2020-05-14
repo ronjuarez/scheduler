6695b566d6ba64f0a85a627cd397f2e1d8d2d859
Ron Juarez <ronstewartjuarez@gmail.com>
2020-05-14 12:57:33 -0400

    removed mock days data -- fetched api data

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,25 +1,10 @@
-import React, { useState } from "react";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 import "components/Application.scss";
 import DayList from "components/DayList";
 import Appointment from "components/Appointment";
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
+
 
 const appointments =[
   {
@@ -67,7 +52,17 @@ const appointments =[
 
 
 export default function Application(props) {
-  const [day, setDay] = useState('Monday')
+  const [day, setDay] = useState("Monday")
+  const [days, setDays] =useState([])
+  
+  useEffect(() => {
+    axios({
+      method: "GET",
+      url: `http://localhost:8001/api/days`})
+      .then((response) => {
+        setDays(response.data);
+    })
+  }, []);
   
   const schedule = appointments.map((appointment) => {
     return (
