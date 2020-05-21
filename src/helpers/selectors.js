cbb33a5d6568d9313e342389461a4049e41fec9c
Ron Juarez <ronstewartjuarez@gmail.com>
2020-05-21 12:45:13 -0400

    comments for selctor functions

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,3 +1,18 @@
+
+
+// This is a function that takes two parameters the state object and the 
+// day state which is set to Monday as default. Here is how it works.
+//
+// 1. It takes the days array that is stored in the state object and filters it to only
+//    return the day with the name property that matches the day in the days array and stores this 
+//    value in our filteredDays variable.
+// 2. a) If the length of the filteredDays array is not 0 then select the day at index 0, which should be
+//    our only day, and take the array stored in the appointments property and mutates each item of
+//    our appointments into individual arrays and stores them in the appointments property of our state.
+//    b) Else return an empty array
+// 3  We store the value the return of this condition as the value of variable sortedApp and return it as
+//    the final value of getAppointmentsForDay.
+
 export function getAppointmentsForDay(state, day) {
   const filteredDays = state.days.filter(days => days.name === day);
 
@@ -6,11 +21,25 @@ export function getAppointmentsForDay(state, day) {
   return sortedApp;
 }
 
+// This is a function that takes two parameters the state object and the 
+// day interview object which is set to Monday as default. Here is how it works:
+//
+// 1. a) If no interview object is passed it returns null. Otherwise if the 
+//    interview item does exist we're going to create an object that contains two
+//    properties.
+//    b) The first property is a student key that takes the value of the student property
+//    of our interview object. The second property is an interviewer key that takes 
+//    the value of the state object's interviewers property and returns the value at
+//    key of the value of our interviewer property of our interview object.
+//
+// 2. It stores the resulting object of the function in a variable called intDetails
+//    and returns it as the result of the function call.    
 export function getInterview(state, interview) {
   const intDetails = !interview ? null : { student: interview.student, interviewer: state.interviewers[interview.interviewer] };
   return intDetails;
 };
 
+// This function is identical to getAppointmentsByDay except it works with interview values.
 export function getInterviewersForDay(state, day) {
   const filteredDays = state.days.filter(days => days.name === day);
 
